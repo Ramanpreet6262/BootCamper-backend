5fec6dbc7106fc1c10731892ff342388f703e60f
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-01-21 20:13:13 +0530

    added logic for sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,7 +15,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ['select'];
+  const removeFields = ['select', 'sort'];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
@@ -33,6 +33,14 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
+  }
+
+  // Sort  (For sorting a +ve value means ascending and a -ve one means descending)
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt'); // Descending of createdAt
   }
 
   // const bootcamps = await Bootcamp.find();
