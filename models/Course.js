019693b15861816d0ad7ae288d073017b1823aeb
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-01-24 00:31:22 +0530

    Average Cost Calculation

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,46 @@ const CourseSchema = new mongoose.Schema({
   }
 });
 
+// Statics and Methods in mongoose
+/* Statics are functions that we directly run on models 
+eg:  Course.goHaveFun() 
+whereas Methods are functions that we run on query/result returned by model 
+eg: const hello = Course.find();
+hello.goHaveFun(); */
+
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  console.log('Calculating avg cost...'.blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call getAverageCost after save
+CourseSchema.post('save', function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+CourseSchema.pre('remove', function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
